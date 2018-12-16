195ad8d6dcdd272457e60672403561a8567b5223
dragan1810 <d.novakovic1810@yahoo.com>
2018-12-16 16:17:42 +0100

    qqq

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,4 @@ const app: Application = express();
 app.use("/", Routes);
 
 const PORT = 3000;
-app.listen(PORT, () => console.log("Server Runing !!!!"));
+app.listen(PORT, () => console.log(`Server Runing !!!! on port:${PORT}`));
