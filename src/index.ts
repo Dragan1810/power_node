756c78530056f20078af1e599d2860031e5b4a6f
dragan1810 <d.novakovic1810@yahoo.com>
2018-12-24 20:23:34 +0100

    added mongo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import express, { Application } from "express";
+import mongoose from 'mongoose'
 import { Routes } from "./routes";
 
 const app: Application = express();
+
+mongoose.connect("mongodb://mongo:27017/power_node");
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
 
 app.use("/", Routes);
 
